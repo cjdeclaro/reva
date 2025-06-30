edd9a4d205b9cd5aeffa00358f2727e85cc4a616
cjdeclaro <cjdeclaro16@gmail.com>
2025-06-30 21:49:43 +0800

    Fix routing tests

--- test/controllers/nps_scores_controller_test.rb
+++ test/controllers/nps_scores_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class NpsScoresControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get nps_scores_index_url
+    get nps_scores_path
     assert_response :success
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get reviews_index_url
+    get reviews_path
     assert_response :success
   end
 end
