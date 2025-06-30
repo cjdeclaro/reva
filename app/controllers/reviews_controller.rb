a8adfda14b75f5dab0e53b83f1640664b46e49ed
cjdeclaro <cjdeclaro16@gmail.com>
2025-06-30 15:08:28 +0800

    Base filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,18 @@ class ReviewsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @q = Review.all
-    @q = @q.where(rating: params[:ratings]) if params[:ratings].present?
-    @q = @q.where(channel: params[:channels]) if params[:channels].present?
-    @q = @q.where(company_name: params[:company_name]) if params[:company_name].present?
-    @q = @q.where(review_date: params[:from_date]..params[:to_date]) if params[:from_date].present? && params[:to_date].present?
-    @q = @q.where("description ILIKE ?", "%#{params[:q]}%") if params[:q].present?
+    @ratings     = Review.distinct.pluck(:rating).sort
+    @channels    = Review.distinct.pluck(:channel).sort
+    @companies   = Review.distinct.pluck(:company_name).sort
 
-    @pagy, @reviews = pagy(@q.order(review_date: :desc))
+    reviews = Review.all
+
+    reviews = reviews.where(rating: params[:ratings]) if params[:ratings].present?
+    reviews = reviews.where(channel: params[:channels]) if params[:channels].present?
+    reviews = reviews.where(company_name: params[:company]) if params[:company].present?
+    reviews = reviews.where(review_date: params[:from]..params[:to]) if params[:from].present? && params[:to].present?
+    reviews = reviews.where("description ILIKE ?", "%#{params[:q]}%") if params[:q].present?
+
+    @pagy, @reviews = pagy(reviews.order(review_date: :desc))
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js"
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
 pin "nps_chart_controller", to: "controllers/nps_chart_controller.js"
+pin "filter_controller", to: "controllers/filter_controller.js"
